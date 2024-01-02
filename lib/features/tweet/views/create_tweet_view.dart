4c68cebd54333a69d17dc31bc819384145597d4c
seilylook <seilylook@naver.com>
2024-01-02 10:51:50 +0900

    design: create tweet screen

--- lib/features/tweet/views/create_tweet_view.dart
+++ lib/features/tweet/views/create_tweet_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/constants/assets_constants.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
@@ -15,6 +18,14 @@ class CreateTweetView extends ConsumerStatefulWidget {
 }
 
 class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
+  final tweetTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    tweetTextController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserDetailsProvider).value;
@@ -57,13 +68,75 @@ class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
                           backgroundImage:
                               NetworkImage(currentUser.profilePic!),
                           radius: 30,
-                        )
+                        ),
+                        const SizedBox(width: 15),
+                        Expanded(
+                          child: TextField(
+                            controller: tweetTextController,
+                            style: const TextStyle(
+                              fontSize: 22,
+                            ),
+                            decoration: const InputDecoration(
+                              hintText: "What's happening?",
+                              hintStyle: TextStyle(
+                                color: Pallete.greyColor,
+                                fontSize: 22,
+                                fontWeight: FontWeight.w700,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                            maxLines: null,
+                          ),
+                        ),
                       ],
                     )
                   ],
                 ),
               ),
             ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.only(bottom: 10),
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Pallete.greyColor,
+              width: 0.3,
+            ),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(
+                AssetsConstants.galleryIcon,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(
+                AssetsConstants.gifIcon,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(
+                AssetsConstants.emojiIcon,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
